fa9b2c917456b7894477d7b18de6113ffa4ea84e
zil-web <126854462+zil-web@users.noreply.github.com>
2026-05-18 10:50:58 +0700

    User

--- src/main/java/com/deploy/pertemuan11/model/User.java
+++ src/main/java/com/deploy/pertemuan11/model/User.java
@@ -1,4 +1,22 @@
 package com.deploy.pertemuan11.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
+    private String id;
+    private String username;
+    private String password;
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private Profile profile;
 }
