ffb373e86df1801e54da97e34a4978961261a597
zil-web <126854462+zil-web@users.noreply.github.com>
2026-05-18 10:34:44 +0700

    Initial commit

---
+++ src/main/java/com/deploy/pertemuan11/Pertemuan11Application.java
@@ -0,0 +1,13 @@
+package com.deploy.pertemuan11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pertemuan11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pertemuan11Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/deploy/pertemuan11/Pertemuan11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deploy.pertemuan11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pertemuan11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
