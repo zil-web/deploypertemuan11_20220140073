93c2d849e9d9861262e4aa507cca10105dd7f083
zil-web <126854462+zil-web@users.noreply.github.com>
2026-05-18 11:09:20 +0700

    security

--- src/main/java/com/deploy/pertemuan11/security/SecurityConfig.java
+++ src/main/java/com/deploy/pertemuan11/security/SecurityConfig.java
@@ -1,4 +1,36 @@
 package com.deploy.pertemuan11.security;
 
+import com.deploy.pertemuan11.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepository) {
+        return username -> {
+            com.deploy.pertemuan11.model.User user = userRepository.findByUsername(username).
+                    orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+
+            return User
+                    .withUsername(user.getUsername())
+                    .password(user.getPassword())
+                    .authorities("USER")
+                    .build();
+        };
+    }
 }
