94329588509b3f2f0380d22a9260d7efa5f92699
zil-web <126854462+zil-web@users.noreply.github.com>
2026-05-18 10:45:06 +0700

    profile

--- src/main/java/com/deploy/pertemuan11/model/Profile.java
+++ src/main/java/com/deploy/pertemuan11/model/Profile.java
@@ -1,4 +1,23 @@
 package com.deploy.pertemuan11.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@Table(name = "profile")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Profile {
+    @Id
+    private String id;
+    private String nama;
+    private String alamat;
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
 }
---
+++ src/main/java/com/deploy/pertemuan11/repository/ProfileRepository.java
@@ -0,0 +1,4 @@
+package com.deploy.pertemuan11.repository;
+
+public interface ProfileRepository {
+}
