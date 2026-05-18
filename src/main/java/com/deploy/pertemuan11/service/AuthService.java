4817ca81ffb318781e570e8e443e9eb33c862583
zil-web <126854462+zil-web@users.noreply.github.com>
2026-05-18 11:53:00 +0700

    auth

--- src/main/java/com/deploy/pertemuan11/service/AuthService.java
+++ src/main/java/com/deploy/pertemuan11/service/AuthService.java
@@ -1,4 +1,52 @@
 package com.deploy.pertemuan11.service;
 
+import com.deploy.pertemuan11.model.Profile;
+import com.deploy.pertemuan11.model.User;
+import com.deploy.pertemuan11.model.dto.RegisterRequest;
+import com.deploy.pertemuan11.repository.UserRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
 public class AuthService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void  register(RegisterRequest register){
+        User user = User.builder()
+                .id(UUID.randomUUID().toString())
+                .username(request.getusername())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .build();
+
+        Profile profile = Profile.builder()
+                .id(UUID.randomUUID().toString())
+                .nama(request.getNama())
+                .alamat(request.getAlamat())
+                .User(user)
+                .Build();
+
+        user.setProfile(profile);
+
+        userRepository.save(user);
+    }
+
+    public User getLoggedInUser(){
+        Authentication authentication =
+                SecurityContextHolder.getContext().getAuthentication();
+
+        String username = authentication.getName();
+
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("User tidak ditemukan"));
+    }
 }
