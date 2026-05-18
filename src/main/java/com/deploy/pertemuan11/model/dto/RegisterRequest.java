86336293fd397ad61b0e5af03482f8096fa82a0e
zil-web <126854462+zil-web@users.noreply.github.com>
2026-05-18 10:58:51 +0700

    dto

--- src/main/java/com/deploy/pertemuan11/model/dto/RegisterRequest.java
+++ src/main/java/com/deploy/pertemuan11/model/dto/RegisterRequest.java
@@ -1,4 +1,12 @@
 package com.deploy.pertemuan11.model.dto;
 
+import lombok.Data;
+
+@Data
 public class RegisterRequest {
+    private String username;
+    private String password;
+
+    private String nama;
+    private String alamat;
 }
