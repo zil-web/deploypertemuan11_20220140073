e94b895e198b627840a7933525585413c6dfd221
zil-web <126854462+zil-web@users.noreply.github.com>
2026-05-18 10:55:22 +0700

    UserRepository

--- src/main/java/com/deploy/pertemuan11/repository/UserRepository.java
+++ src/main/java/com/deploy/pertemuan11/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package com.deploy.pertemuan11.repository;
 
-public interface UserRepository {
+import com.deploy.pertemuan11.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,String> {
+    Optional<User> findByUsername(String username);
 }
