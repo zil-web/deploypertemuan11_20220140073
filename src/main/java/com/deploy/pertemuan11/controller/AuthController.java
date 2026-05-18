bbd7e02ec8c176601e71ba2bb5a03db2c81c7de6
zil-web <126854462+zil-web@users.noreply.github.com>
2026-05-18 13:18:39 +0700

    auth controller

--- src/main/java/com/deploy/pertemuan11/controller/AuthController.java
+++ src/main/java/com/deploy/pertemuan11/controller/AuthController.java
@@ -1,4 +1,34 @@
 package com.deploy.pertemuan11.controller;
 
+import com.deploy.pertemuan11.model.dto.RegisterRequest;
+import com.deploy.pertemuan11.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
 public class AuthController {
+    private final AuthService authService;
+
+    @GetMapping("/register")
+    public String registerPage(Model model) {
+        model.addAttribute("request", new RegisterRequest());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String register(@ModelAttribute RegisterRequest request) {
+        authService.register(request);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
 }
