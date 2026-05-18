e3d961c1c51116e2ba0759c7b6e59c89f6a3b638
zil-web <126854462+zil-web@users.noreply.github.com>
2026-05-18 13:33:09 +0700

    home controller

--- src/main/java/com/deploy/pertemuan11/controller/HomeController.java
+++ src/main/java/com/deploy/pertemuan11/controller/HomeController.java
@@ -1,4 +1,26 @@
 package com.deploy.pertemuan11.controller;
 
+import com.deploy.pertemuan11.model.User;
+import com.deploy.pertemuan11.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@Controller
+@RequiredArgsConstructor
 public class HomeController {
+
+    private AuthService authService;
+
+    @GetMapping("/home")
+    public String home(Model model) {
+
+        User user = authService.getLoggedInUser();
+
+        model.addAttribute("user", user);
+        model.addAttribute("profile", user.getProfile());
+
+        return "home";
+    }
 }
